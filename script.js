94103510dd6a2e500d8c42011bf713f43dca6da4
nispar2009 <varun.parupalli@gmail.com>
2024-02-05 21:25:13 +0530

    Revamped app after a long time

--- script.js
+++ script.js
@@ -21,63 +21,18 @@ verifyTune = () => {
     document.getElementById("subBoth").disabled = true
 }
 
-secureBeat = str => {
-    const allowed = ["P", "Q", "X"]
-
-    str.split(iterator => {
-        if (allowed.includes(iterator) == false) {
-            return false
-        }
-    })
-
-    return true
-}
-
-verifyBeat = () => {
-    const str = getInput("beat")
-    if (secureBeat(str)) {
-        document.getElementById("subBeat").disabled = false
-        document.getElementById("subBoth").disabled = false
-        return
-    }
-    document.getElementById("subBeat").disabled = true
-    document.getElementById("subBoth").disabled = true
-}
-
 playTune = () => {
     const tune = getInput("tune")
     let count = 0
 
-    document.getElementById("audioTune").innerHTML = `<audio id="melody" autoplay src="Audio/${tune[0]}.m4a"></audio>`
-    document.getElementById("melody").playbackRate = 9
+    document.getElementById("audioTune").innerHTML = `<audio id="melody" autoplay src="Audio/${tune[0]}.wav"></audio>`
+    document.getElementById("melody").playbackRate = 5
     document.getElementById("melody").onended = () => {
         count++
         if (count < tune.length) {
-            document.getElementById("melody").src = `Audio/${tune[count]}.m4a`
-            document.getElementById("melody").playbackRate = 9
+            document.getElementById("melody").src = `Audio/${tune[count]}.wav`
+            document.getElementById("melody").playbackRate = 5
             document.getElementById("melody").play()
         }
     }
-}
-
-playBeat = () => {
-    const beat = getInput("beat")
-    let count = 0
-
-    document.getElementById("audioBeat").innerHTML = `<audio id="bass" autoplay src="Audio/${beat[0]}.m4a"></audio>`
-    document.getElementById("bass").playbackRate = 9
-    document.getElementById("bass").volume = 0.25
-    document.getElementById("bass").onended = () => {
-        count++
-        if (count < beat.length) {
-            document.getElementById("bass").src = `Audio/${beat[count]}.m4a`
-            document.getElementById("bass").playbackRate = 9
-            document.getElementById("bass").play()
-        }
-    }
-}
-
-playBoth = () => {
-    playTune()
-    playBeat()
 }
